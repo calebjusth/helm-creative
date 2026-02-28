0668c4b4806d2c2d12a0a3d5aaf72d3607dbc5bf
calebjusth <calebwesley98@gmail.com>
2026-02-28 17:18:40 +0300

    Updated UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://helmcreative.com.et',
+  base: '/',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
